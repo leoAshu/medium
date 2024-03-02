97b473900fdc079f08d4b26b20f9f90802ddd728
leoAshu <ashutosh.ojha2009@gmail.com>
2024-03-01 19:00:48 -0800

    routing with skeleton pages and quote component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Blog, Signin, Signup } from "./pages"
+
 function App() {
-    return <>Medium</>
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/blog/:id" element={<Blog />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+const Quote = () => {
+    return (
+        <div className="bg-slate-200 h-screen flex flex-col justify-center">
+            <div className="mx-auto max-w-lg">
+                <div className="text-2xl font-semibold">
+                    "The customer service I received was exceptional. The
+                    support team went above and beyond to address my concerns."
+                </div>
+                <div className="my-1 text-lg font-semibold">
+                    Julies Winfield
+                </div>
+                <div className="text-sm font-light text-slate-400">
+                    CEO | Acme Inc
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Quote
