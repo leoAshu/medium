09edc9f346fb716fc62be7ea2ca1cf7644ab77e3
leoAshu <ashutosh.ojha2009@gmail.com>
2024-03-06 19:43:35 -0800

    adds cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { userRouter, blogRouter } from './routes'
 
 const app = new Hono()
+
+app.use('/*', cors())
 
 app.get('/', (c) => {
     return c.json({
